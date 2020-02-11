b036e5579f2791b4f90b0c6a63fac54e2554543e
Jonathan SARDO <sardoj@gmail.com>
2020-02-11 17:32:40 +0100

    fix(layout): Fixes message bar

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,6 +1,14 @@
 @if (session('impersonate.original_user'))
-<div style="padding: 4px 30px; color: white;" class="orange right-align">
+<div style="padding: 4px 30px; color: white; position: fixed; left: 0; right: 0; z-index: 100" class="orange right-align">
     {{ trans('impersonate::message.logged_in_as') }} <b>{{ auth()->user()->name }}</b>.
     <a href="{{ ucroute('impersonate.stop', $domain) }}" class="primary-text" style="margin-left: 16px; font-weight: bold">{{ trans('impersonate::message.stop_impersonation') }}</a>
 </div>
 @endif
+
+@section('extra-css')
+<style>
+body.theme-uccello .navbar-fixed.navbar-top {
+    margin-top: 30px;
+}
+</style>
+@append
